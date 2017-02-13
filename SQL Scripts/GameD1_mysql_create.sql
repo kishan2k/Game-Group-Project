5e97b1a4e7cd4079ada4736048aff1d54e467598
Peter Robertson <rober380@uni.coventry.ac.uk>
2017-02-13 10:29:14 +0000

    Update GameD1 Create script
    Updated the GameD1 database create SQL script to allow a 32x32 map to be inserted.

--- SQL Scripts/GameD1_mysql_create.sql
+++ SQL Scripts/GameD1_mysql_create.sql
@@ -4,7 +4,7 @@ CREATE TABLE `Player` (
 	`Health` INT(16) NOT NULL,
 	`Strenght` INT(16) NOT NULL,
 	`CurrentScore` INT(32) NOT NULL,
-	`LevelsID` INT(8) NOT NULL,
+	`Level` INT(8) NOT NULL,
 	`EnergyLevel` INT(16) NOT NULL,
 	PRIMARY KEY (`PlayerID`)
 );
@@ -21,7 +21,7 @@ CREATE TABLE `High_Score` (
 CREATE TABLE `Levels` (
 	`LevelsID` INT(8) NOT NULL,
 	`LevelNumber` INT(8) NOT NULL,
-	`Map` VARCHAR(64) NOT NULL,
+	`Map` VARCHAR(1024) NOT NULL,
 	PRIMARY KEY (`LevelsID`)
 );
 
@@ -55,7 +55,7 @@ CREATE TABLE `Levels_Enemy` (
 	PRIMARY KEY (`LevelEnemyID`)
 );
 
-ALTER TABLE `Player` ADD CONSTRAINT `Player_fk0` FOREIGN KEY (`LevelsID`) REFERENCES `Levels`(`LevelsID`);
+ALTER TABLE `Player` ADD CONSTRAINT `Player_fk0` FOREIGN KEY (`Level`) REFERENCES `Levels`(`LevelsID`);
 
 ALTER TABLE `High_Score` ADD CONSTRAINT `High_Score_fk0` FOREIGN KEY (`PlayerID`) REFERENCES `Player`(`PlayerID`);
 
